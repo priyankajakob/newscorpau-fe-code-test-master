05efeca266d036773267f0f96b5de4bcb7a7e2c4
Priyanka Jacob <priyanka.jacob93@gmail.com>
2023-06-13 16:14:29 +0530

    fix:made ArticleThumbnail reusable

--- src/pages/articles/List/ArticleThumbnail.jsx
+++ src/pages/articles/List/ArticleThumbnail.jsx
@@ -1,24 +1,12 @@
 import { MuiBox } from "../../../components/atoms";
 import { Thumbnail } from "../../../components/molecules";
 
-const ArticleThumbnail = ({article}) => {
-    const loadThumbnailImages = (article) => {
-        //TODO: Recheck thumbnail logic and use lodash here
-        const idd = article.related?.thumbnail?.default[0] || "";
-        const thumbnailUrl = article.references[idd]?.link?.media || "";
-    
-        const image = {
-          alt: article.extendedHeadline?.default,
-          src: thumbnailUrl,
-          height: article?.references[idd]?.height || "75",
-          width: article?.references[idd]?.width || "100",
-        };
-        return <Thumbnail image={image} />;
-      };
-
+const ArticleThumbnail = ({image}) => {
     return(
-        <MuiBox>{loadThumbnailImages(article)}</MuiBox>
-    )
+        <MuiBox>
+          <Thumbnail image={image} />
+        </MuiBox>
+    ) 
 }
 
 export default ArticleThumbnail;
