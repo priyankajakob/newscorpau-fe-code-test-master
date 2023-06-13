0649f0eb443121d27b4008557d2881088e4073ff
Priyanka Jacob <priyanka.jacob93@gmail.com>
2023-06-13 17:27:36 +0530

    feat:load data using fetch api and handle loading/error message on screen

--- src/constants/common.constant.js
+++ src/constants/common.constant.js
@@ -1,1 +1,3 @@
 export const PAGE_404_ERROR = "The requested page was not found."
+export const LOADING = "Loading..."
+export const NETWORK_ERROR = "Something went wrong while fetching the data. Please try again later"
--- src/pages/articles/List/index.jsx
+++ src/pages/articles/List/index.jsx
@@ -2,16 +2,38 @@ import { useState, useEffect } from "react";
 import { MuiBox, MuiTypography } from "../../../components/atoms";
 import { Content } from "../../../components/molecules";
 import { ArticleSection } from "../../../components/molecules/articles";
-import CurrentList from "../../../data/capi.json";
+import { commonConstants } from '../../../constants'
 import "./list.scss";
 
 const ArticlesList = () => {
   const [fullArticlesSet, setFullArticlesSet] = useState({});
   const [articlesList, setArticlesList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
-    setFullArticlesSet(CurrentList);
+    setLoading(true);
   }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        //This could be replaced with any api call instead of mocked json
+        const response = await fetch("./capi.json");
+        const fullData = await response.json();
+        console.log(fullData);
+        setFullArticlesSet(fullData);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+        setError(true);
+        setLoading(false);
+      }
+    }
+    if (loading) {
+      fetchData();
+    }
+  }, [loading]);
 
   useEffect(() => {
     if (fullArticlesSet.hits > 0 && fullArticlesSet?.results) {
@@ -25,11 +47,16 @@ const ArticlesList = () => {
         {/* Todo: Add Pagination */}
         <MuiTypography variant={"h2"}> </MuiTypography>
         <MuiBox>
-          {articlesList.map((article) => {
-            //TODO: Remove this console.log
-            console.log(article);
-            return <ArticleSection article={article} />;
-          })}
+          {loading && <p>{commonConstants.LOADING}</p>}
+          {error && <p>{commonConstants.NETWORK_ERROR}</p>}
+          {!error &&
+            !loading &&
+            articlesList.map((article) => {
+              //TODO: Remove this console.log
+              console.log(article);
+
+              return <ArticleSection article={article} />;
+            })}
         </MuiBox>
       </MuiBox>
     </Content>
