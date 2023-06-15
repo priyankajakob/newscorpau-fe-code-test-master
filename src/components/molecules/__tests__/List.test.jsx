91c845c79ab2e80bc3e54d58f8bcfb7154bbd1d5
Priyanka Jacob <priyanka.jacob93@gmail.com>
2023-06-16 00:15:20 +0530

    fix:fix test case with context

--- src/components/molecules/__tests__/List.test.jsx
+++ src/components/molecules/__tests__/List.test.jsx
@@ -2,11 +2,12 @@ import { render, screen } from "@testing-library/react";
 import List from "../List";
 import { commonConstants } from "../../../constants";
 import mockListData from './sampleArticlesList.json';
+import { ArticleContext } from '../../../context';
 
 describe("Test for List component", () => {
 
   it("Test 1: Checking if error is shown in case of api error", () => {
-    render(<List error/>);
+    render(<ArticleContext.Provider value={{error:true,loading:false}}><List/></ArticleContext.Provider>);
     
     const errorReg = new RegExp(commonConstants.NETWORK_ERROR)
     const htmlElement = screen.getByText(errorReg)
@@ -14,7 +15,7 @@ describe("Test for List component", () => {
   })
 
   it("Test 2: Checking if loading message is shown in case of page initial loading", () => {
-    render(<List loading/>);
+    render(<ArticleContext.Provider value={{loading:true,error:false}}><List/></ArticleContext.Provider>);
     
     const loadingReg = new RegExp(commonConstants.LOADING)
     const htmlElement = screen.getByText(loadingReg)
@@ -22,7 +23,7 @@ describe("Test for List component", () => {
   })
 
   it("Test 2: Checking if only 4 articles are rendered on first page", () => {
-    render(<List list={mockListData}/>);
+    render(<ArticleContext.Provider value={{list:mockListData}}><List/></ArticleContext.Provider>);
     
     const nextPageReg = new RegExp(mockListData[4].headline)
     expect(() => screen.getByText(nextPageReg)).toThrow()
